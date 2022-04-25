ce2585574e43ef36f73886c1acf8ab0fbd83df67
Albert <102740257+AlbertikW@users.noreply.github.com>
2022-04-25 17:12:51 +0500

    test
    with changes1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 let menuBtn = document.querySelector('.menu-btn');
 let header_nav = document.querySelector('.header_nav');
 let header_logo = document.querySelector('.header_logo');
-let logo_button2 = document.querySelector('.logo_button2');
+let hidden_menu = document.querySelector('.hidden_menu');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	header_nav.classList.toggle('active');
 	header_logo.classList.toggle('active');
-	logo_button2.classList.toggle('active');
+	hidden_menu.classList.toggle('active');
 })
